f04face0fd4d8090fc6965dcfa36d86128704559
PrashantRPatil <prashantpatil745@gmail.com>
2023-06-05 23:00:31 +0530

    created optimized production build

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
-import './App.css';
-import React, {useEffect,useState} from "react";
-import Weather from './components/weather';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Weather from "./components/weather";
+import { Dimmer, Loader } from "semantic-ui-react";
+
 export default function App() {
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
-const [lat, setLat] = useState([]);
-const [long, setLong] = useState([]);
-const [data, setData] = useState([]);
-
-useEffect(() => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const fetchData = async () => {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    setLat(position.coords.latitude);
-    setLong(position.coords.longitude);
-  });
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position?.coords?.latitude);
+      setLong(position?.coords?.longitude);
+    });
 
-  await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-      .then(res => res.json())
-      .then(result => {
-        setData(result)
+    await fetch(
+      `${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
         console.log(result);
       });
-    }
-      fetchData();
-    }, [lat, long])
-  
+  };
 
+  useEffect(() => {
+    fetchData();
+  }, [fetchData, lat, long]);
 
   return (
     <div className="App">
        {(typeof data.main != 'undefined') ? (
         <Weather weatherData={data}/>
       ): (
-        <div></div>
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
       )}
-     
+
     </div>
   );
+
+  
 }
-
-
